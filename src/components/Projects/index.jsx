fd02cc30b1f307be1898ff3823fd03712308f8b9
“Taaati” <taaatimachado@gmail.com>
2024-04-05 20:22:48 -0300

    Updating l List

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -4,6 +4,8 @@ import CarouselItem from "./carousel-item";
 import pokedex from '../../assets/pokedex.jpg'
 import ilovepxl from '../../assets/ilovepxl.jpg'
 import portfolio from '../../assets/devportfolio.jpg'
+import coffeebara from '../../assets/coffeebara.jpg'
+
 
 
 const Projects = () => {
@@ -11,9 +13,12 @@ const Projects = () => {
         <div id="projects" className=" pt-10 px-2 max-w-[1440px] mx-auto">
             <Shiny className="text-2xl my-0 pt-10 px-2 text-center">Projects</Shiny>
             <Carousel show={1} slide={1} transition={0.5} swiping={true} responsive className="m-auto">
-                <CarouselItem img={pokedex} url={'https://github.com/Felipe-Streva/Pokedex_API'} title='Pokédex'/>
-                <CarouselItem img={ilovepxl} url={'https://github.com/TaaatiMachado/ilovepixel'} title='I Love Pixel'/>
-                <CarouselItem img={portfolio} url={'https://github.com/TaaatiMachado/Portfolio'} title='Portfolio'/>
+                <CarouselItem img={coffeebara} url={'https://github.com/TaaatiMachado/coffeebara'} title='Coffeebara App' />
+                <CarouselItem img={ilovepxl} url={'https://github.com/TaaatiMachado/ilovepixel'} title='I Love Pixel' />
+                <CarouselItem img={portfolio} url={'https://github.com/TaaatiMachado/Portfolio'} title='Portfolio' />
+                <CarouselItem img={pokedex} url={'https://github.com/Felipe-Streva/Pokedex_API'} title='Pokédex' />
+
+
             </Carousel>
         </div>
     )
